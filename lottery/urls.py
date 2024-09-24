981b8e2e634a05f80b50303fb7b27eae25e0e428
Aprilx02862 <phanujarus0802@icloud.com>
2024-09-24 18:57:59 +0700

    s

--- lottery/urls.py
+++ lottery/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView, LotteryDetailView
+from .views import IndexView, LotteryDetailView , LotterryBills
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('bet-detail/<int:pk>/', LotteryDetailView.as_view(), name='lottery_detail'),
-    
+    path('bills/',LotterryBills.as_view(), name='bills'),
+
 ]
